d1c5370d7fb46f21ff02193bf8d224fd5f7c0d55
Eren Araz <lerenarazl@gmail.com>
2025-07-20 02:02:38 +0300

    bench: 2275840

--- src/search.cpp
+++ src/search.cpp
@@ -271,8 +271,7 @@ Value Worker::search(Position& pos, Stack* ss, Value alpha, Value beta, Depth de
             reduction -= PV_NODE;
             Depth reduced_depth = std::clamp<Depth>(new_depth - reduction, 1, new_depth);
             value = -search<false>(pos_after, ss + 1, -alpha - 1, -alpha, reduced_depth, ply + 1);
-            if (value > alpha && reduced_depth < new_depth) {
-                
+            if (value > alpha && reduced_depth < new_depth) {                
                 new_depth -= move_history / 16384;
                 value = -search<false>(pos_after, ss + 1, -alpha - 1, -alpha, new_depth, ply + 1);
             }
