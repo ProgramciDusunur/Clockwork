45319aca4667f2adf6a0aeb813de33e195b828b0
Eren Araz <lerenarazl@gmail.com>
2025-08-16 18:29:28 +0300

    bench: 1591941

--- src/search.cpp
+++ src/search.cpp
@@ -279,7 +279,7 @@ Value Worker::search(Position& pos, Stack* ss, Value alpha, Value beta, Depth de
                 // if the move have good history decrease reduction other hand the move have bad history then reduce more
                 auto move_history_reduction = move_history / 8192;                
                 reduction -= move_history_reduction;
-            }
+            }            
             Depth reduced_depth = std::clamp<Depth>(new_depth - reduction, 1, new_depth);
             value = -search<false>(pos_after, ss + 1, -alpha - 1, -alpha, reduced_depth, ply + 1);
             if (value > alpha && reduced_depth < new_depth) {
