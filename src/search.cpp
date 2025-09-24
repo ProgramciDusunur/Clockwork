1f88c6009011ea55fccd1c85a35cc206fd6a04bf
Eren Araz <lerenarazl@gmail.com>
2025-09-24 17:37:16 +0300

    tweak and try again

--- src/search.cpp
+++ src/search.cpp
@@ -488,9 +488,10 @@ Value Worker::search(
 
         // Singular extensions
         int extension = 0;
-        if (!excluded && tt_data && m == tt_data->move && depth >= 6 && tt_data->depth >= depth - 3
+        if (!excluded && tt_data && m == tt_data->move && depth >= 7 && tt_data->depth >= depth - 3
             && tt_data->bound & Bound::Lower) {
-            Value singular_beta  = tt_data->score - 4 * depth;            
+            Value singular_beta  = tt_data->score - 2 * depth;
+            
             int   singular_depth = depth / 2;
 
             ss->excluded_move    = m;
