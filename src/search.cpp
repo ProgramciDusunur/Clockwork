74b5bd6516c968c25e7ce8211eecef4809d726f8
Eren Araz <lerenarazl@gmail.com>
2025-10-09 16:22:07 +0300

    doctor, the patient, was have so many search explosions, the merged stuff should be fixed it, right?

--- src/search.cpp
+++ src/search.cpp
@@ -535,6 +535,11 @@ Value Worker::search(
                 if (!PV_NODE && singular_value <= singular_beta - 40) {
                     extension = 2;
                 }
+
+                // Triple Extension
+                if (quiet && singular_value <= singular_beta - 120) {
+                    extension = 3;
+                }
             }
 
             // Multicut
