2d89423669abe28b2713143ea70314a6b3e4ffe9
Eren Araz <lerenarazl@gmail.com>
2025-10-09 04:13:39 +0300

    bench: 28907404

--- src/search.cpp
+++ src/search.cpp
@@ -438,7 +438,6 @@ Value Worker::search(
 
     bool tt_pv_decision = tt_data && ttpv && tt_data->score >= beta + 90 - 15 * ((tt_data->depth + depth) / 2);
 
-
     if (!PV_NODE && !is_in_check && tt_pv_decision && depth <= tuned::rfp_depth && !excluded
         && tt_adjusted_eval >= beta + tuned::rfp_margin * depth) {
         return tt_adjusted_eval;
