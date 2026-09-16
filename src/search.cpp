5003dba74013b0855b986ec627c4b51504e55ac3
Eren Araz <lerenarazl@gmail.com>
2026-09-16 15:13:57 +0300

    give more bonus to history updates if static eval failed low

--- src/search.cpp
+++ src/search.cpp
@@ -1039,6 +1039,10 @@ Value Worker::search(
 
     if (best_value >= beta) {
         i32       bonus_depth = depth + (best_value >= beta + 100);
+        // static eval failed low, 
+        // we should give more priority for the best move and penalty for other moves
+        bonus_depth += (!is_in_check && tt_adjusted_eval <= alpha);
+        
         const i32 bonus       = stat_bonus(bonus_depth);
         const i32 malus       = stat_malus(bonus_depth);
         if (quiet_move(best_move)) {
